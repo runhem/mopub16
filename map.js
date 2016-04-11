58783a50d192388424e00189fc86498c4bf8d974
runhem <alexandra@ep.se>
2016-04-11 16:59:08 +0200

    zoom och pan :)

--- map.js
+++ map.js
@@ -3,7 +3,9 @@ var initialize = function() {
 	var mapOptions = {
 		center: { lat: 59.332382, lng: 18.0645235 },
 		zoom: 18,
-		tilt: 45
+		tilt: 45,
+		disableDefaultUI:true
+
 	};
 map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 };
@@ -14,5 +16,8 @@ var zoom = function(num) {
 	map.setZoom(zoom + num);
 }
 
+var pan = function(a, b){
+	var pan = map.panBy(a, b);
+}
 
 google.maps.event.addDomListener(window, 'load', initialize);
